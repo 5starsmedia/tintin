d2009dea92da3ad645284380bc150ee08061efc2
Vitalii Savchuk <esvit666@gmail.com>
2015-04-01 10:30:38 +0300

    app resource

--- api/services/tasks/plugins/visaDates.js
+++ api/services/tasks/plugins/visaDates.js
@@ -4,13 +4,18 @@ var async = require('async'),
   mongoose = require('mongoose'),
   _ = require('lodash');
 
+function checkVisa(model, callback) {
+  console.info(model);
+}
+
 exports['visaDates.check'] = function (app, msg, cb) {
   console.info('visaDates.check');
 
   app.models.visaDates.find({ isEnabled: true }, function(err, visas) {
     if (err) { return cb(err); }
     console.info(visas);
-
-    cb();
+    async.map(visas, checkVisa, function(err, results){
+      cb();
+    });
   });
 };
