1b5d9fb43493078d0712fe176f122c9a03186eb6
Vitalii Savchuk <esvit666@gmail.com>
2015-04-22 10:39:44 +0300

    fix exp

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -72,7 +72,7 @@ module.exports = function () {
     var token = req.headers.authorization.split(' ')[1];
     if (token && token.length > 0) {
       var payload = jwt.decode(token, config.get('auth.tokenSecret'));
-      if (payload.exp <= moment().unix()) {
+      if (!payload || payload.exp <= moment().unix()) {
         authAsGuest(req, res, next);
         return;
       }
