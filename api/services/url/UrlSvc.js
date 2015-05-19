11a970cf003d5491d40a681647da51ca5dc238e4
Vitalii Savchuk <esvit666@gmail.com>
2015-05-19 10:03:10 +0300

    add url service

---
+++ api/services/url/UrlSvc.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var _ = require('lodash'),
+  slug = require('limax');
+
+function UrlSvc(app) {
+  this.app = app;
+}
+
+UrlSvc.prototype.aliasFor = function (app, text, options, cb) {
+  if (!text) { return cb(new Error('Text for alias is empty')); }
+  options = _.defaults(_.clone(options || {}), {maxLength: 200});
+  var result = slug(text, { lang: 'ru' });
+  result = result.substr(0, options.maxLength);
+  cb(null, result);
+};
+
+UrlSvc.prototype.urlFor = function (collectionName, params) {
+  switch (collectionName) {
+    case 'posts':
+      return this.app.config.get('url') + '/' + params.category.parentAlias + '/' + params.category.alias + '/' + params.alias + '.html';
+  }
+};
+
+
+module.exports = UrlSvc;
--- api/services/url/index.js
+++ api/services/url/index.js
@@ -1,26 +1,3 @@
-/**
- * Url helpers
- * @module services/url
- * @copyright 2014 Cannasos.com. All rights reserved.
- */
 'use strict';
 
-var _ = require('lodash'),
-  slug = require("limax");
-
-exports.aliasFor = function (app, text, options, cb) {
-  if (!text) { return cb(new Error('Text for alias is empty')); }
-  options = _.defaults(_.clone(options || {}), {maxLength: 200});
-  var result = slug(text);
-  result = result.substr(0, options.maxLength);
-  cb(null, result);
-};
-
-exports.urlFor = function(collectionName, params) {
-
-  switch (collectionName) {
-    case 'posts':
-      return 'http://' + params.site.domain + '/' + params.category.parentAlias + '/' + params.category.alias + '/' + params.alias + '.html';
-  }
-
-};
+exports.UrlSvc = require('./UrlSvc.js');
