7f5d201acffe18006318bfd5e0457b9a01715016
Vitalii Savchuk <esvit666@gmail.com>
2015-05-19 14:36:32 +0300

    add seo field

--- app/modules/news/models/NewsPostModel.js
+++ app/modules/news/models/NewsPostModel.js
@@ -4,7 +4,7 @@ export default
     var resource = $resource('/api/posts/:_id/:method', {
       '_id': '@_id'
     }, {
-      'get': {method: 'GET', params: { fields: 'id,title,alias,body,category,status,tags,source,files,coverFile,createDate,isAllowComments,photoSource,ownPhoto,isTop,isHighlight,meta,keywords,keywordGroup' }},
+      'get': {method: 'GET', params: { fields: 'id,title,alias,body,seo,category,status,tags,source,files,coverFile,createDate,isAllowComments,photoSource,ownPhoto,isTop,isHighlight,meta,keywords,keywordGroup' }},
       'save': {method: 'PUT'},
       'create': {method: 'POST'},
       'scanSeo': {method: 'POST', params: { method: 'seo-scan' } }
