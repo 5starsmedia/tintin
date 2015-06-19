ae433c12f525965b8185368dd09ca1b8d461d3ff
Vitalii Savchuk <esvit666@gmail.com>
2015-06-19 13:43:01 +0300

    add menu

--- api/services/tasks/plugins/visaDates.js
+++ api/services/tasks/plugins/visaDates.js
@@ -77,7 +77,7 @@ function checkVisa(app, model, callback) {
     'thirdPage': ['secondPage', function(next, resData) {
 
       var res = url.resolve(host + path, action);
-      console.info(res, host + path, action);
+      console.info(res);
       request.post({ url: res, form: resData.secondPage, headers: { 'Referer': host + path } }, function(err,response,body){
 
         path = response.request.uri.path;
