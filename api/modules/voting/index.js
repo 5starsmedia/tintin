e749f7867fb99fc1ee5f56cd7583e228267150ae
Vitalii Savchuk <esvit666@gmail.com>
2015-07-03 14:13:25 +0300

    add test

--- api/modules/voting/index.js
+++ api/modules/voting/index.js
@@ -12,9 +12,19 @@ VotingModule.prototype.initModels = function () {
 
 VotingModule.prototype.initRoutes = function () {
 
+  var self = this;
   this.app.services.broadcast.on('socket:send.vote', function (data) {
 
     console.info(data);
+
+    async.auto({
+      'poll': function (next) {
+        self.app.models.polls.findById(msg.body._id, next);
+      }
+    }, function (err, res) {
+      if (err) { return cb(err); }
+      res.account.save(cb);
+    });
 
   });
 };
