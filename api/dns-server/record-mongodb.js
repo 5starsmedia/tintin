7aab2286ffe638c4eb8d075014043e49c44b6b7b
Vitalii Savchuk <esvit666@gmail.com>
2015-08-13 07:51:06 +0530

    dns

--- api/dns-server/record-mongodb.js
+++ api/dns-server/record-mongodb.js
@@ -1,83 +1,86 @@
 var config = require('../config.js'),
   MongoClient = require('mongodb').MongoClient,
-  format = require('util').format;
+  format = require('util').format,
+  db;
 
-MongoClient.connect(config.get('mongodb'), function (err, db) {
+MongoClient.connect(config.get('mongodb'), function (err, database) {
   if (err) {
     return err;
   }
-
-  exports.queryRecord = function (name, type, callback) {
-    var collection = db
-      .collection('dnsRecords')
-      .find({
-        'domain.name': name.toLowerCase(),
-        type: type
-      })
-      .toArray(function (err, docs) {
-        if (err) {
-          return callback(err, null);
-        }
-        callback(null, docs);
-      });
-  };
-
-  exports.queryGeo = function (name, type, dest, isp, sourceIP, callback) {
-    if (dest === null) {
-      return callback(null);
-    }
-    var collection = db
-      .collection('dnsRecords')
-      .find({
-        'domain.name': name.toLowerCase(),
-        type: type/*,
-        geo: {
-          $in: [
-            dest.country_code,
-            dest.country_code3,
-            dest.country_name,
-            dest.continent_code
-          ]
-        }*/
-      })
-      .toArray(function (err, docs) {
-        if (err) {
-          return callback(err, null);
-        }
-        callback(null, docs);
-      });
-  };
-
-  exports.querySOA = function (name, callback) {
-    exports.queryRecord(name, 'SOA', callback);
-  };
-
-  exports.queryNS = function (name, callback) {
-    exports.queryRecord(name, 'NS', callback);
-  };
-
-  exports.queryA = function (name, callback) {
-    exports.queryRecord(name, 'A', callback);
-  };
-
-  exports.queryAAAA = function (name, callback) {
-    exports.queryRecord(name, 'AAAA', callback);
-  };
-
-  exports.queryCNAME = function (name, callback) {
-    exports.queryRecord(name, 'CNAME', callback);
-  };
-
-  exports.queryMX = function (name, callback) {
-    exports.queryRecord(name, 'MX', callback);
-  };
-
-  exports.querySRV = function (name, callback) {
-    exports.queryRecord(name, 'SRV', callback);
-  };
-
-  exports.queryTXT = function (name, callback) {
-    exports.queryRecord(name, 'TXT', callback);
-  };
+  db = database;
 });
 
+
+
+exports.queryRecord = function (name, type, callback) {
+  var collection = db
+    .collection('dnsRecords')
+    .find({
+      'domain.name': name.toLowerCase(),
+      type: type
+    })
+    .toArray(function (err, docs) {
+      if (err) {
+        return callback(err, null);
+      }
+      callback(null, docs);
+    });
+};
+
+exports.queryGeo = function (name, type, dest, isp, sourceIP, callback) {
+  if (dest === null) {
+    return callback(null);
+  }
+  var collection = db
+    .collection('dnsRecords')
+    .find({
+      'domain.name': name.toLowerCase(),
+      type: type/*,
+       geo: {
+       $in: [
+       dest.country_code,
+       dest.country_code3,
+       dest.country_name,
+       dest.continent_code
+       ]
+       }*/
+    })
+    .toArray(function (err, docs) {
+      if (err) {
+        return callback(err, null);
+      }
+      callback(null, docs);
+    });
+};
+
+exports.querySOA = function (name, callback) {
+  exports.queryRecord(name, 'SOA', callback);
+};
+
+exports.queryNS = function (name, callback) {
+  exports.queryRecord(name, 'NS', callback);
+};
+
+exports.queryA = function (name, callback) {
+  exports.queryRecord(name, 'A', callback);
+};
+
+exports.queryAAAA = function (name, callback) {
+  exports.queryRecord(name, 'AAAA', callback);
+};
+
+exports.queryCNAME = function (name, callback) {
+  exports.queryRecord(name, 'CNAME', callback);
+};
+
+exports.queryMX = function (name, callback) {
+  exports.queryRecord(name, 'MX', callback);
+};
+
+exports.querySRV = function (name, callback) {
+  exports.queryRecord(name, 'SRV', callback);
+};
+
+exports.queryTXT = function (name, callback) {
+  exports.queryRecord(name, 'TXT', callback);
+};
