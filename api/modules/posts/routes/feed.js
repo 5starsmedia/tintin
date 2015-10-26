ffd45a410711575570a9d53a80c211bcde0c9207
Vitalii Savchuk <esvit666@gmail.com>
2015-10-26 09:29:16 +0200

    fix feed

--- api/modules/posts/routes/feed.js
+++ api/modules/posts/routes/feed.js
@@ -5,21 +5,22 @@ var express = require('express'),
   _ = require('lodash'),
   async = require('async'),
   rss = require('rss'),
+  htmlToText = require('html-to-text'),
   router = express.Router();
 
 router.get('/', function (req, res, next) {
   async.auto({
     'rss': function(next) {
       var feed = new rss({
-        title: 'Новини Вінниці',
+        title: req.site.title || '',
         description: '',
         feed_url: req.app.config.get('url') + req.request.url,
         site_url: req.app.config.get('url'),
-        image_url: 'http://news.vn.ua/assets/img/rss_logo.png',
-        managingEditor: 'info@news.vn.ua (Редакція)',
-        webMaster: 'esvit666@gmail.com (Vitalii Savchuk)',
-        language: 'uk',
-        //categories: ['Category 1','Category 2','Category 3'],
+        //image_url: 'http://news.vn.ua/assets/img/rss_logo.png',
+        //managingEditor: 'info@news.vn.ua (Редакція)',
+        //webMaster: 'esvit666@gmail.com (Vitalii Savchuk)',
+        //language: 'uk',
+        //]]categories: ['Category 1','Category 2','Category 3'],
         pubDate: new Date(),
         ttl: '60',
         custom_namespaces: {
@@ -32,14 +33,15 @@ router.get('/', function (req, res, next) {
       req.app.models.posts.find({ status: 4, removed: { $exists: false } }, next);
     },
     'items': ['posts', 'rss', function(next, data) {
-      _.forEach(data, function(item) {
+      _.forEach(data.posts, function(item) {
+        var text = htmlToText.fromString(item.body, {wordwrap: 120, tables: false, ignoreHref: true, ignoreImage: true});
         data.rss.item({
           title: item.title,
-          description: '<p>use this for the content. It can include html.',
-          url: 'http://example.com/article4?this&that', // link to the item
-          categories: ['Category 1','Category 2','Category 3','Category 4'], // optional - array of item categories
-          author: 'Guest Author', // optional - defaults to feed author property
-          date: 'May 27, 2012', // any format that js Date can parse.
+          description: text,
+          url: req.site.url + req.app.services.url.urlFor('posts', item), // link to the item
+          categories: [item.category.title], // optional - array of item categories
+          author: item.account.title, // optional - defaults to feed author property
+          date: item.createDate, // any format that js Date can parse.
           custom_elements: [
             {'yandex:full-text': item.body},
             {'yandex:genre': 'article'}
