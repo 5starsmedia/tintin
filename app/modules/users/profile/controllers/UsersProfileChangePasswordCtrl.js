a5d3c893d7f69be0928522df6094d112079907c7
Vitalii Savchuk <esvit666@gmail.com>
2015-10-30 07:49:11 +0200

    add change password

--- app/modules/users/profile/controllers/UsersProfileChangePasswordCtrl.js
+++ app/modules/users/profile/controllers/UsersProfileChangePasswordCtrl.js
@@ -2,11 +2,24 @@ export default
 class UsersProfileChangePasswordCtrl {
 
   /*@ngInject*/
-  constructor($scope, user, UsersProfileSrv) {
-    $scope.account = user;
+  constructor($scope, user, UsersProfileSrv, notify) {
+    $scope.user = user;
     /*
     UsersProfileSrv.getAccountInfo(function(info){
       $scope.account = info;
     });*/
+
+    $scope.saveItem = function(item) {
+      $scope.loading = true;
+
+      item.$save((data) => {
+        $scope.loading = false; 
+
+        notify({
+          message: 'Пароль изменен',
+          classes: 'alert-success'
+        });
+      });
+    };
   }
 }
