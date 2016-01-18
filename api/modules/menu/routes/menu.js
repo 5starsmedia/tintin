527f7b5be16cc52def8986b8e38c76d24f6f3ea7
Vitalii Savchuk <esvit666@gmail.com>
2016-01-18 08:44:44 +0200

    add comments avatar

--- api/modules/menu/routes/menu.js
+++ api/modules/menu/routes/menu.js
@@ -10,7 +10,10 @@ router.get('/mainmenu', function (req, res, next) {
 
   async.auto({
     'mainMenu': function(next) {
-      return req.app.models.menuElements.findOne({ 'site._id': req.site._id, isMainMenu: true }, next);
+      if (req.site._id == 'footerMenu' || req.site._id == 'mainMenu') {
+        return req.app.models.menuElements.findOne({ 'menuType': req.site._id, removed: { $exists: false } }, next);
+      }
+      return req.app.models.menuElements.findOne({ 'site._id': req.site._id, isMainMenu: true, removed: { $exists: false } }, next);
     }
   }, function (err, data) {
     if (err) { return next(err); }
