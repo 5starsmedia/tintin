09ca829484705f409ad617566c97a969dbce2e6f
Vitalii Savchuk <esvit666@gmail.com>
2016-02-20 12:18:50 +0200

    update redirects

--- api/services/tasks/plugins/categories.js
+++ api/services/tasks/plugins/categories.js
@@ -9,26 +9,39 @@ var async = require('async'),
     _ = require('lodash');
 
 exports['db.categories.update'] = function (app, msg, cb) {
+    var categories = {};
     async.auto({
         'category': function(next) {
             app.models.categories.findById(msg.body._id, next);
         },
-        'descendants': ['category', function(next, data) {
-            data.category.getDescendants(next);
-        }],
-        'posts': ['category', 'descendants', function(next, data) {
-            var ids = _.pluck(data.descendants, '_id');
-            ids.push(msg.body._id);
-            app.models.posts.find({ 'category._id': { '$in': ids } }, next);
-        }],
         'site': ['category', function(next, data) {
             app.models.sites.findById(data.category.site._id, next);
         }],
+        'descendants': ['category', function(next, data) {
+            data.category.getDescendants(next);
+        }],
+        'categories': ['category', 'descendants', 'site', function(next, data) {
+            async.each(data.descendants, function(item, next) {
+                var urlFrom = app.services.url.urlFor('categories', item);
+                item.parentAlias = data.category.alias;
+                var urlTo = app.services.url.urlFor('categories', item);
+
+                categories[item._id] = item;
+                item.save(function() {
+                    app.services.redirects.newRedirect(urlFrom, urlTo, data.site, next);
+                });
+            }, next);
+        }],
+        'posts': ['category', 'descendants', 'categories', function(next, data) {
+            var ids = _.pluck(data.descendants, '_id');
+            ids.push(msg.body._id);
+            categories[msg.body._id] = data.category;
+            app.models.posts.find({ 'category._id': { '$in': ids } }, next);
+        }],
         'checkUrl': ['posts', 'category', 'site', function(next, res) {
-            console.info(res.posts)
             async.each(res.posts, function(item, next) {
                 var urlFrom = app.services.url.urlFor('posts', item);
-                item.category = res.category;
+                item.category = categories[item.category._id];
                 var urlTo = app.services.url.urlFor('posts', item);
 
                 item.save(function() {
