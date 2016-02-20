9fa8ea5a051a6aa6575780c7cd695f54c39f16bb
Vitalii Savchuk <esvit666@gmail.com>
2016-02-20 12:02:06 +0200

    update redirects

--- api/services/tasks/plugins/categories.js
+++ api/services/tasks/plugins/categories.js
@@ -10,21 +10,27 @@ var async = require('async'),
 
 exports['db.categories.update'] = function (app, msg, cb) {
     async.auto({
-        'posts': function(next) {
-            app.models.posts.find({ 'category._id': msg.body._id }, next);
-        },
         'category': function(next) {
             app.models.categories.findById(msg.body._id, next);
         },
+        'descendants': ['category', function(next, data) {
+            data.category.getDescendants(next);
+        }],
+        'posts': ['category', 'descendants', function(next, data) {
+            var ids = _.pluck(data.descendants, '_id');
+            ids.push(msg.body._id);
+            app.models.posts.find({ 'category._id': { '$in': ids } }, next);
+        }],
         'site': ['category', function(next, data) {
             app.models.sites.findById(data.category.site._id, next);
         }],
         'checkUrl': ['posts', 'category', 'site', function(next, res) {
+            console.info(res.posts)
             async.each(res.posts, function(item, next) {
                 var urlFrom = app.services.url.urlFor('posts', item);
                 item.category = res.category;
                 var urlTo = app.services.url.urlFor('posts', item);
-console.info(urlFrom, urlTo)
+
                 item.save(function() {
                     app.services.redirects.newRedirect(urlFrom, urlTo, res.site, next);
                 });
